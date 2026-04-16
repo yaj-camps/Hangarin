818b1f1898e0d03018ca1c1ba16b3ad23d18f2ea
jay <poscam53@gmail.com>
2026-04-16 15:38:32 +0800

    PWA

--- projectsite/settings.py
+++ projectsite/settings.py
@@ -107,8 +107,8 @@ ACCOUNT_SIGNUP_FIELDS = [
     "password2*",
 ]
 
-PWA_APP_NAME = 'ProjectSite'
-PWA_APP_DESCRIPTION = "A Progressive Web App version of ProjectSite"
+PWA_APP_NAME = 'Hangarin'
+PWA_APP_DESCRIPTION = "A Progressive Web App version of Hangarin"
 PWA_APP_THEME_COLOR = '#0A0A0A'
 PWA_APP_BACKGROUND_COLOR = '#FFFFFF'
 PWA_APP_DISPLAY = 'standalone'
@@ -117,24 +117,24 @@ PWA_APP_ORIENTATION = 'portrait'
 PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
-{
-'src': '/static/img/logo2.png',
-'sizes': '192x192'
-},
-{
-'src': '/static/img/logo1.png',
-'sizes': '512x512'
-}
+    {
+        'src': '/static/img/icon-192.png',
+        'sizes': '192x192'
+    },
+    {
+        'src': '/static/img/icon-512.png',
+        'sizes': '512x512'
+    }
 ]
 PWA_APP_ICONS_APPLE = [
-{
-'src': '/static/img/logo2.png',
-'sizes': '192x192'
-},
-{
-'src': '/static/img/logo1.png',
-'sizes': '512x512'
-}
+    {
+        'src': '/static/img/icon-192.png',
+        'sizes': '192x192'
+    },
+    {
+        'src': '/static/img/icon-512.png',
+        'sizes': '512x512'
+    }
 ]
 PWA_APP_DIR = 'ltr'
 PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'static/js', 'serviceworker.js')
